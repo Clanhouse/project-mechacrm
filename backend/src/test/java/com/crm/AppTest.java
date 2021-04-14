eb33052df6d29ca85242bb71ebcd140a2f5ba5c1
Mateusz <mateusz.skrzypczyk1@hotmail.com>
2021-04-14 22:00:38 +0200

    Generated backend project

---
+++ backend/src/test/java/com/crm/AppTest.java
@@ -0,0 +1,12 @@
+package com.crm;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
