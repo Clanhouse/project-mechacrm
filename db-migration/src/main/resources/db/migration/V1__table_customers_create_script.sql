be60d9ec2215c5910e3b9777f0cf64e3b2a9d0ab
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-16 18:06:57 +0200

    adding script for create customers table

---
+++ db-migration/src/main/resources/db/migration/V1__table_customers_create_script.sql
@@ -0,0 +1,10 @@
+CREATE TABLE customers
+(
+    id      int8         primary key not null unique generated always as identity ,
+    name    varchar(100) not null,
+    surname varchar(100) not null,
+    phone   varchar(30)  not null unique,
+    address varchar(255) not null
+);
+
+
