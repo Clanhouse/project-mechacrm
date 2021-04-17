9e80a9be3e06636cbfa85630e7685df3e7766861
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-17 16:58:22 +0200

    theme object with basic struct for colors and font-size, font-weight

---
+++ frontend/src/theme/theme.js
@@ -0,0 +1,20 @@
+const theme = {
+  color: {
+    white: '#ffffff',
+    black: '#000000',
+    grey: '#d8d8d8',
+  },
+
+  fontSize: {
+    s: '14px',
+    m: '15px',
+    l: '16px',
+  },
+
+  fontWeight: {
+    light: '300',
+    bold: '600',
+  },
+};
+
+export default theme;
