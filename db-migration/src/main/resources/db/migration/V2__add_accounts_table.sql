0e1fbbd7dbbce3732402b85465e66f4f1615b69a
Artur C <cwiklinski.artur@wp.pl>
2021-04-17 11:34:04 +0200

    add flyway script for create accounts, roles table

---
+++ db-migration/src/main/resources/db/migration/V2__add_accounts_table.sql
@@ -0,0 +1,20 @@
+CREATE TABLE roles(
+    role_id BIGSERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE accounts(
+    account_id BIGSERIAL PRIMARY KEY,
+    login TEXT NOT NULL UNIQUE,
+    password TEXT,
+    email TEXT NOT NULL UNIQUE,
+    login_attempts INT,
+    registration_date TIMESTAMP,
+    last_successful_login TIMESTAMP,
+    last_failed_login TIMESTAMP,
+    role_id INT,
+
+    CONSTRAINT fk_role
+        FOREIGN KEY(role_id)
+            REFERENCES roles(role_id)
+);
