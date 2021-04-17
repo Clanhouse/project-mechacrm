c7a286dba6a8984fa04b4b882d726d1f19a19057
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-17 16:32:48 +0200

    removed default content of App.js, added GlobalStyles

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GlobalStyles from './views/GlobalStyles';
 
 const App = () => (
-  <div className="Appa">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        <code>src/App.js</code>
-        and save to reload.
-      </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </a>
-    </header>
-  </div>
+  <>
+    <GlobalStyles />
+    <div>global style</div>
+  </>
 );
 
 export default App;
