4d6da3866f3ad953f3a2e4ef4ac2e41495b8adc4
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-17 02:27:21 +0200

    IconButton story

---
+++ frontend/src/components/atoms/IconButton/IconButton.stories.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import IconButton from './IconButton';
+import { ReactComponent as IconAlarm } from '../../../assets/svgs/alarm-light.svg';
+import { ReactComponent as IconCalendar } from '../../../assets/svgs/calendar-range.svg';
+
+export default {
+  title: 'Atoms/IconButton',
+  component: IconButton,
+};
+
+const Template = (args) => <IconButton {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  label: 'icon button',
+  icon: <IconAlarm />,
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'icon button',
+  color: '#ffe800',
+  icon: <IconCalendar />,
+};
