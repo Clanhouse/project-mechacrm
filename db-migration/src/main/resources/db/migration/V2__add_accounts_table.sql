e5d652e22037ae691aa9f14b62e0203c5affddaa
Artur C <cwiklinski.artur@wp.pl>
2021-04-18 20:04:14 +0200

    added IF NOT EXISTS, removed unnecessary spaces, changed column types

--- db-migration/src/main/resources/db/migration/V2__add_accounts_table.sql
+++ db-migration/src/main/resources/db/migration/V2__add_accounts_table.sql
@@ -1,19 +1,17 @@
-CREATE TABLE roles(
-    role_id BIGSERIAL PRIMARY KEY,
-    name TEXT UNIQUE NOT NULL
+CREATE TABLE IF NOT EXISTS roles(
+    role_id SERIAL PRIMARY KEY,
+    name VARCHAR(50) UNIQUE NOT NULL
 );
-
-CREATE TABLE accounts(
+CREATE TABLE IF NOT EXISTS accounts(
     account_id BIGSERIAL PRIMARY KEY,
-    login TEXT NOT NULL UNIQUE,
-    password TEXT,
-    email TEXT NOT NULL UNIQUE,
+    login VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255),
+    email VARCHAR(100) NOT NULL UNIQUE,
     login_attempts INT,
     registration_date TIMESTAMP,
     last_successful_login TIMESTAMP,
     last_failed_login TIMESTAMP,
     role_id INT,
-
     CONSTRAINT fk_role
         FOREIGN KEY(role_id)
             REFERENCES roles(role_id)
