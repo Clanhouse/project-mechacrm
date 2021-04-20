22eac0d1ea70767ed5c35f21a4e6633cab0468a9
ell96p <epelka9@gmail.com>
2021-04-20 18:58:30 +0200

    adding new migration and renaming previous one(incorrect number)

--- db-migration/src/main/resources/db/migration/V3__relational_table_customers_cars.sql
+++ db-migration/src/main/resources/db/migration/V3__relational_table_customers_cars.sql
@@ -1,11 +1,8 @@
-create table customers_cars
-
+CREATE TABLE IF NOT EXISTS customers_cars
 (
-    id INT PRIMARY KEY UNIQUE GENERATED ALWAYS AS IDENTITY,
-    customers_id bigint not null
+    id           INT PRIMARY KEY UNIQUE GENERATED ALWAYS AS IDENTITY,
+    customers_id bigint  not null
         references customers,
-
-    car_id integer not null
+    car_id       integer not null
         references cars
-
 );
