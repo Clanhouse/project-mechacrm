fc3c8e2165cb8dd3ef01df03997fd33ec00b12c8
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-24 15:52:53 +0200

    [CRM-23] added some bootstrap colors and Roboto as font family

--- frontend/src/theme/MainTheme.js
+++ frontend/src/theme/MainTheme.js
@@ -2,23 +2,34 @@ const theme = {
   color: {
     primary: '#007bff',
     secondary: '#6c757d',
+    info: '#17a2b8',
+    success: '#28a745',
+    warning: '#ffc107',
+    danger: '#dc3545',
+    dark: '#343a40',
+    light: '#f8f9fa',
     textColor: '#ccc',
     active: '#eee',
     white: '#ffffff',
     black: '#000000',
     grey: '#d8d8d8',
+    grey100: 'hsl(0, 0%, 96%)',
+    grey200: 'hsl(0, 0%, 90%)',
+    grey300: 'hsl(0, 0%, 70%)',
   },
 
   fontSize: {
-    s: '14px',
-    m: '15px',
-    l: '16px',
+    s: '12px',
+    m: '18px',
+    l: '24px',
   },
 
   fontWeight: {
     light: '300',
     bold: '600',
   },
+
+  fontFamily: "'Roboto', sans-serif",
 };
 
 export default theme;
