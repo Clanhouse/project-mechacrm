7baeb69b4d2dc2f974b4b27e3ecef2172d840ed2
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-24 15:26:27 +0200

    [CRM-23] added ThemeProvider to main app and as global decorator for all stories

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import GlobalStyles from './theme/GlobalStyles';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './theme/MainTheme';
 
 const App = () => (
   <>
     <GlobalStyles />
-    <div>Hello world!</div>
+    <ThemeProvider theme={theme}>
+      <div>Hello world!</div>
+    </ThemeProvider>
   </>
 );
 
