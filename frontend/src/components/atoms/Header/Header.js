d233bd507cb6d5b3549684d2544e882fbb011c04
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-04-29 00:02:56 +0200

    [CRM-23] Header styles update for background color and margin bottom

--- frontend/src/components/atoms/Header/Header.js
+++ frontend/src/components/atoms/Header/Header.js
@@ -3,19 +3,19 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Container = styled.div`
-  margin: 24px;
+  background-color: ${({ theme }) => theme.color.white};
+  margin-bottom: 2px;
 `;
 
 const Title = styled.div`
   padding: 16px 24px;
-  
   text-transform: capitalize;
   font-family: ${({ theme }) => theme.fontFamily};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
   font-size: ${({ theme }) => theme.fontSize.l};
   color: ${({ theme }) => theme.color.primary};
   
-  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;;
+  box-shadow: rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px;
 `;
 
 const Header = ({ title }) => (
