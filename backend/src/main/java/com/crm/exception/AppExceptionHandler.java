c1483f614b2990d82e416b9f7b493ee55d28ea70
ell96p <epelka9@gmail.com>
2021-05-16 16:22:59 +0200

    CRM 33 - ExceptionHandler

--- backend/src/main/java/com/crm/exception/AppExceptionHandler.java
+++ backend/src/main/java/com/crm/exception/AppExceptionHandler.java
@@ -14,13 +14,13 @@ public class AppExceptionHandler {
     public static final String SOURCE_ERROR_MESSAGE = "Unknown source";
 
     @ExceptionHandler
-    public ResponseEntity<ErrorResponse> handleException (Exception exc, HttpServletRequest request){
+    public ResponseEntity<ErrorResponse> handleException (final Exception exc, final HttpServletRequest request){
 
         return new ResponseEntity<>(new ErrorResponse(HttpStatus.BAD_REQUEST.value(),getSource(request),-1, exc.getMessage()),
         HttpStatus.BAD_REQUEST);
     }
 
-    private String getSource(HttpServletRequest request) {
+    private String getSource(final HttpServletRequest request) {
         String source;
         try {
             source = request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
