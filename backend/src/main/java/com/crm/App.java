84e4589cad3dc35b53f142b71f11ef37347efead
Fellway <mateusz.skrzypczyk1@hotmail.com>
2021-06-10 15:44:47 +0200

    Added PidFileWriter

--- backend/src/main/java/com/crm/App.java
+++ backend/src/main/java/com/crm/App.java
@@ -2,12 +2,15 @@ package com.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 public class App {
     public static void main(String[] args) {
-        SpringApplication.run(App.class, args);
+        SpringApplication springApplication = new SpringApplication(App.class);
+        springApplication.addListeners(new ApplicationPidFileWriter());
+        springApplication.run(args);
     }
 }
