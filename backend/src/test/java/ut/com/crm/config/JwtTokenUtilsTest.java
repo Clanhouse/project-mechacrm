5660cfec25511d6932730eaa9f9bca64a01d4b69
Fellway <mateusz.skrzypczyk1@hotmail.com>
2021-06-13 14:50:08 +0200

    [CRM-43] Removed unnecessary comments

--- backend/src/test/java/ut/com/crm/config/JwtTokenUtilsTest.java
+++ backend/src/test/java/ut/com/crm/config/JwtTokenUtilsTest.java
@@ -23,57 +23,54 @@ class JwtTokenUtilsTest {
 
     @Test
     void shouldReturnNotEmptyToken() {
-        //given:
         JwtTokenUtils jwtTokenUtils = new JwtTokenUtils();
         jwtTokenUtils.setRefreshTokenExpirationTime(REFRESH_TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setTokenExpirationTime(TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setSecret(SECRET);
-        //when:
+
         String token = jwtTokenUtils.generateToken(userDetails);
-        //then:
+
         assertThat(token).isNotBlank().isNotEmpty();
     }
 
     @Test
     void shouldReturnNotEmptyRefreshedToken() {
-        //given:
         JwtTokenUtils jwtTokenUtils = new JwtTokenUtils();
         jwtTokenUtils.setRefreshTokenExpirationTime(REFRESH_TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setTokenExpirationTime(TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setSecret(SECRET);
-        //when:
+
         String token = jwtTokenUtils.generateRefreshToken(userDetails);
-        //then:
+
         assertThat(token).isNotBlank().isNotEmpty();
     }
 
     @Test
     void shouldReturnUserNameEqualToExample() {
-        //given:
+
         JwtTokenUtils jwtTokenUtils = new JwtTokenUtils();
         jwtTokenUtils.setRefreshTokenExpirationTime(REFRESH_TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setTokenExpirationTime(TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setSecret(SECRET);
-        //when:
+
         when(userDetails.getUsername()).thenReturn("example");
         String token = jwtTokenUtils.generateToken(userDetails);
         String userName = jwtTokenUtils.getUsername(token);
-        //then:
+
         assertThat(userName).isEqualTo("example");
     }
 
     @Test
     void shouldReturnExpirationDateEqualToTOKEN_EXPIRATION_TIME() {
-        //given:
         JwtTokenUtils jwtTokenUtils = new JwtTokenUtils();
         jwtTokenUtils.setRefreshTokenExpirationTime(REFRESH_TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setTokenExpirationTime(TOKEN_EXPIRATION_TIME);
         jwtTokenUtils.setSecret(SECRET);
-        //when:
+
         when(userDetails.getUsername()).thenReturn("example");
         String token = jwtTokenUtils.generateToken(userDetails);
         Boolean valid = jwtTokenUtils.isValid(token, userDetails);
-        //then:
+
         Assertions.assertTrue(valid);
     }
 }
