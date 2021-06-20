3ef71ad7ce602e5c78c0ff3654f6ed2d118b5f05
Fellway <mateusz.skrzypczyk1@hotmail.com>
2021-06-20 23:27:12 +0200

    [CRM-56] Better formatting for SQL scripts

--- db-migration/src/main/resources/db/migration/V3__relational_table_customers_cars.sql
+++ db-migration/src/main/resources/db/migration/V3__relational_table_customers_cars.sql
@@ -1,5 +1,4 @@
-CREATE TABLE IF NOT EXISTS customers_cars(
-  id INT8 PRIMARY KEY UNIQUE GENERATED ALWAYS AS IDENTITY,
-  customers_id INT8 not null references customers(id),
-  car_id INT8 not null references cars(id)
-);
+CREATE TABLE IF NOT EXISTS customers_cars (
+	id           INT8 PRIMARY KEY UNIQUE GENERATED ALWAYS AS IDENTITY,
+	customers_id INT8 NOT NULL REFERENCES customers (id),
+	car_id       INT8 NOT NULL REFERENCES cars (id));
