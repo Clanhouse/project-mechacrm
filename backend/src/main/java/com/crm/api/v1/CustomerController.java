a5cce7cd7fb5e8f098d17e0ef18211f4c699136b
Andrzej Sikorski <42008783+darkFlame666@users.noreply.github.com>
2021-06-27 11:36:35 +0200

    CRM-59 (#44)

--- backend/src/main/java/com/crm/api/v1/CustomerController.java
+++ backend/src/main/java/com/crm/api/v1/CustomerController.java
@@ -1,16 +1,17 @@
 package com.crm.api.v1;
 
+import com.crm.dto.request.PageRequest;
 import com.crm.dto.response.CustomerResponse;
 import com.crm.service.CustomerService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -22,7 +23,7 @@ public class CustomerController {
     @GetMapping
     @ApiOperation(value = "Finds all customers paginated", notes = "Add param \"page\" and/or \"size\""
             + " to specify page no. and size of each page.")
-    public ResponseEntity<Page<CustomerResponse>> getCustomersPaginated(@PageableDefault final Pageable pageable) {
-        return ResponseEntity.ok(customerService.getCustomersPaginated(pageable));
+    public ResponseEntity<Page<CustomerResponse>> getCustomersPaginated(@Valid PageRequest pageRequest) {
+        return ResponseEntity.ok(customerService.getCustomersPaginated(pageRequest.getPage(), pageRequest.getSize()));
     }
 }
--- backend/src/main/java/com/crm/dto/request/PageRequest.java
+++ backend/src/main/java/com/crm/dto/request/PageRequest.java
@@ -9,8 +9,8 @@ import javax.validation.constraints.Min;
 @Getter
 @Setter
 public class PageRequest {
-    @Min(value = 0, message = ErrorDict.CARS_PAGINATED_INVALID_PAGE)
+    @Min(value = 0, message = ErrorDict.PAGINATED_INVALID_PAGE)
     private Integer page = 0;
-    @Min(value = 1, message = ErrorDict.CARS_PAGINATED_INVALID_SIZE)
+    @Min(value = 1, message = ErrorDict.PAGINATED_INVALID_SIZE)
     private Integer size = 20;
 }
