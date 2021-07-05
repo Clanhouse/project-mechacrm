69a567278b5576d28ed5d8d68b520813dd3052eb
Andrzej Sikorski <42008783+darkFlame666@users.noreply.github.com>
2021-07-05 21:05:28 +0200

    Crm 54 (#45)
    * [CRM-54] GetCarById endpoint
    
    Co-authored-by: Fellway <mateusz.skrzypczyk1@hotmail.com>

--- backend/src/main/java/com/crm/exception/ErrorDict.java
+++ backend/src/main/java/com/crm/exception/ErrorDict.java
@@ -1,6 +1,9 @@
 package com.crm.exception;
 
 public class ErrorDict {
+
+    public static final String CAR_NOT_FOUND = "Car with provided id doesn't exist";
     public static final String PAGINATED_INVALID_PAGE = "Page has to be greater or equal to 0";
     public static final String PAGINATED_INVALID_SIZE = "Size has to be greater or equal to 1";
+
 }
--- backend/src/main/java/com/crm/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/com/crm/exception/GlobalExceptionHandler.java
@@ -24,6 +24,13 @@ public class GlobalExceptionHandler {
         return new ErrorResponse(getSource(request), errorMessages);
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(CarNotFoundException.class)
+    public ErrorResponse handleCarNotFoundException(final CarNotFoundException e, final HttpServletRequest request) {
+        final String errorMessage = e.getMessage();
+        return new ErrorResponse(getSource(request), List.of(errorMessage));
+    }
+
     private String getSource(final HttpServletRequest request) {
         try {
             return request.getAttribute(HandlerMapping.PATH_WITHIN_HANDLER_MAPPING_ATTRIBUTE).toString();
