fdd41a943018b7f707e4ab6a3c828f4908da0096
aliwocha <arturliwocha@gmail.com>
2021-07-20 22:25:20 +0200

    CRM-74 Delete code generating application.pid file

--- backend/src/main/java/com/crm/App.java
+++ backend/src/main/java/com/crm/App.java
@@ -2,7 +2,6 @@ package com.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -10,7 +9,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class App {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(App.class);
-        springApplication.addListeners(new ApplicationPidFileWriter());
         springApplication.run(args);
     }
 }
