b43c58bfa834ad3e2692e2665bf7fd079bf7f7da
aliwocha <arturliwocha@gmail.com>
2021-07-26 22:09:24 +0200

    CRM-69 Add unit tests for method getCustomersPaginated + small fixes

--- backend/src/test/java/com/crm/service/impl/CustomerServiceImplTest.java
+++ backend/src/test/java/com/crm/service/impl/CustomerServiceImplTest.java
@@ -12,7 +12,14 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static io.jsonwebtoken.lang.Assert.notNull;
@@ -31,12 +38,17 @@ public class CustomerServiceImplTest {
     private CustomerRepository customerRepository;
 
     @Mock
-    private CustomerMapper mapper;
+    private CustomerMapper customerMapper;
 
     @InjectMocks
     private CustomerServiceImpl customerService;
 
     private static CustomerEntity customerEntity;
+    private static CustomerResponse customerResponse;
+    private static Pageable pageable;
+
+    private static final int PAGE = 0;
+    private static final int SIZE = 20;
 
     @BeforeClass
     public static void setUp() {
@@ -47,16 +59,20 @@ public class CustomerServiceImplTest {
                 .phone(PHONE)
                 .address(ADDRESS)
                 .build();
+
+        customerResponse = new CustomerResponse();
+        pageable = PageRequest.of(PAGE, SIZE);
     }
 
     @Test
     public void shouldGetCustomerById() {
         when(customerRepository.findById(ID)).thenReturn(Optional.of(customerEntity));
-        when(mapper.convertToDto(customerEntity)).thenReturn(new CustomerResponse());
+        when(customerMapper.convertToDto(customerEntity)).thenReturn(customerResponse);
 
         final CustomerResponse response = customerService.getCustomerById(ID);
 
         verify(customerRepository, times(1)).findById(ID);
+        verify(customerMapper, times(1)).convertToDto(customerEntity);
         notNull(response);
     }
 
@@ -67,6 +83,20 @@ public class CustomerServiceImplTest {
         customerService.getCustomerById(ID);
 
         verify(customerRepository, times(1)).findById(ID);
-        verify(mapper, times(0)).convertToDto(any());
+        verify(customerMapper, times(0)).convertToDto(any());
+    }
+
+    @Test
+    public void shouldGetCustomersPaginated() {
+        List<CustomerEntity> customerEntities = new ArrayList<>(Collections.singletonList(customerEntity));
+
+        when(customerRepository.findAll(pageable)).thenReturn(new PageImpl<>(customerEntities, pageable, 1L));
+        when(customerMapper.convertToDto(customerEntity)).thenReturn(customerResponse);
+
+        final Page<CustomerResponse> response = customerService.getCustomersPaginated(PAGE, SIZE);
+
+        verify(customerRepository, times(1)).findAll(pageable);
+        verify(customerMapper, times(1)).convertToDto(customerEntity);
+        notNull(response);
     }
 }
