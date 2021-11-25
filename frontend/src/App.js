aded157a624ae5e0f1e645bfc555bae8c3556e83
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2021-11-26 00:07:48 +0100

    [CRM-64] added LoginPage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import {
+  BrowserRouter, Route, Switch,
+} from 'react-router-dom';
+import LoginPage from 'pages/LoginPage';
+import HomePage from 'pages/HomePage';
+import NotFoundPage from 'pages/NotFoundPage';
 import GlobalStyles from './theme/GlobalStyles';
 
 import theme from './theme/MainTheme';
 
 const App = () => (
-  <>
+  <BrowserRouter>
     <GlobalStyles />
     <ThemeProvider theme={theme}>
-      <div>Hello world!</div>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/login" exact component={LoginPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </ThemeProvider>
-  </>
+  </BrowserRouter>
 );
 
 export default App;
