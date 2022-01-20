1f3d88bb735a72da2afd6059031f60f88d68af98
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-01-20 23:09:58 +0100

    [CRM-92] update of MainTheme

--- frontend/src/theme/MainTheme.js
+++ frontend/src/theme/MainTheme.js
@@ -1,27 +1,15 @@
 const theme = {
   color: {
-    background: {
-      yellow: '#ffb400',
-    },
-    text: {
-      primary: '#04294F',
-    },
-    primary: '#007bff',
-    secondary: '#6c757d',
+    primary: '#ffb400',
+    secondary: '#04294f',
+    dark: '#ccc7c7',
+    light: '#f4f0f0',
+    white: '#ffffff',
+    black: '#000000',
     info: '#17a2b8',
     success: '#28a745',
     warning: '#ffc107',
     danger: '#dc3545',
-    dark: '#343a40',
-    light: '#f8f9fa',
-    textColor: '#ccc',
-    active: '#eee',
-    white: '#ffffff',
-    black: '#000000',
-    grey: '#d8d8d8',
-    grey100: 'hsl(0, 0%, 96%)',
-    grey200: 'hsl(0, 0%, 90%)',
-    grey300: 'hsl(0, 0%, 70%)',
   },
 
   fontSize: {
@@ -29,21 +17,13 @@ const theme = {
     m: '18px',
     l: '24px',
     xl: '32px',
-    xxl: '48px',
+    xxl: '42px',
   },
 
   fontWeight: {
     light: '300',
     regular: '400',
     bold: '700',
-  },
-
-  fontFamily: "'Roboto', sans-serif",
-
-  shadows: {
-    s: '0px 3px 5px -1px rgba(0,0,0,0.2),0px 5px 8px 0px rgba(0,0,0,0.14),0px 1px 14px 0px rgba(0,0,0,0.12)',
-    m: '0px 6px 6px -3px rgba(0,0,0,0.2),0px 10px 14px 1px rgba(0,0,0,0.14),0px 4px 18px 3px rgba(0,0,0,0.12)',
-    l: '0px 8px 9px -5px rgba(0,0,0,0.2),0px 15px 22px 2px rgba(0,0,0,0.14),0px 6px 28px 5px rgba(0,0,0,0.12)',
   },
 };
 
