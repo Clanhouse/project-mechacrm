e34f414f79701620a03b53b77fab3749c2cee8b5
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-01-20 22:51:07 +0100

    [CRM-92] update of GlobalStyles

--- frontend/src/theme/GlobalStyles.js
+++ frontend/src/theme/GlobalStyles.js
@@ -5,9 +5,33 @@ const GlobalStyles = createGlobalStyle`
     box-sizing: border-box;
   }
 
+  * {
+    margin: 0;
+    padding: 0;
+  }
+
   body {
     margin: 0;
     padding: 0;
+    min-height: 100vh;
+    font-family: "Roboto", sans-serif;
+    font-size: 18px;
+    color: #04294F;
+  }
+
+  img, picture, video, canvas, svg {
+    display: block;
+    max-width: 100%;
+  }
+  input, button, textarea, select {
+    font: inherit;
+  }
+  p, h1, h2, h3, h4, h5, h6 {
+    overflow-wrap: break-word;
+  }
+
+  a {
+    text-decoration: none;
   }
 `;
 
