1f66a4120f74f8e461853471024548ae94bd4511
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-01-24 19:09:58 +0100

    [CRM-92] added RegisterPage.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,8 +7,9 @@ import LoginPage from 'pages/LoginPage';
 import HomePage from 'pages/HomePage';
 import NotFoundPage from 'pages/NotFoundPage';
 import GlobalStyles from 'theme/GlobalStyles';
-
 import theme from 'theme/MainTheme';
+import RegisterPage from 'pages/RegisterPage';
+import AccountCreatedPage from 'pages/AccountCreatedPage';
 
 const App = () => (
   <BrowserRouter>
@@ -17,6 +18,9 @@ const App = () => (
       <Routes>
         <Route path="/" exact element={<HomePage />} />
         <Route path="/login" exact element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />}>
+          <Route path="created" element={<AccountCreatedPage />} />
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
