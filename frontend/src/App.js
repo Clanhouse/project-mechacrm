138bcf546f0a0fd30c63b04f568e06fdfc35c1cf
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-02-28 22:40:15 +0100

    [CRM-92] HomePage after review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import {
   BrowserRouter, Routes, Route,
 } from 'react-router-dom';
 import LoginPage from 'pages/LoginPage';
-import HomePage from 'pages/HomePage';
 import NotFoundPage from 'pages/NotFoundPage';
 import GlobalStyles from 'theme/GlobalStyles';
 import theme from 'theme/MainTheme';
 import RegisterPage from 'pages/RegisterPage';
+import HomePage from 'pages/HomePage';
+import Dashboard from 'pages/Dashboard';
 
 const App = () => (
   <BrowserRouter>
@@ -18,6 +19,7 @@ const App = () => (
         <Route path="/" exact element={<HomePage />} />
         <Route path="/login" exact element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </ThemeProvider>
