b692ee6562a371efb628bcede889498cbe8dd5fd
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-08-14 14:16:31 +0200

    MTO-176 absolute imports

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './pages/App';
+import App from 'pages/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
