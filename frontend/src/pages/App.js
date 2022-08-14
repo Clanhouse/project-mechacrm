ab03da665455a60fb2d7a7c7a1fe8d7dcff1bd40
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-08-14 14:52:47 +0200

    MTO-176 react-router with simple MainPage.js and NotFoundPage.js

--- frontend/src/pages/App.js
+++ frontend/src/pages/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import GlobalStyles from 'theme/GlobalStyles';
 import { ThemeProvider } from 'styled-components';
+import { Route, Routes } from 'react-router-dom';
 import lightTheme from '../theme/lightTheme';
+import MainPage from './MainPage';
+import NotFoundPage from './NotFoundPage';
 
 function App() {
   return (
     <ThemeProvider theme={lightTheme}>
       <GlobalStyles />
-      <div>Motomo</div>
+      <Routes>
+        <Route path='/' element={<MainPage />} />
+        <Route path='*' element={<NotFoundPage />} />
+      </Routes>
     </ThemeProvider>
   );
 }
