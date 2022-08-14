f4ce7263e66d9660db194ec622ec6c59534956a5
Kris <magikabdul@users.noreply.github.com>
2022-08-14 13:04:46 +0200

    Feature/mto 177 main project from scratch (#94)
    * MTO-177 project cleaning
    
    * MTO-177 added frontend using create-react-app v5.0.1
    
    * MTO-177 added backend
    
    * MTO-177 readme initial state

--- README.md
+++
@@ -1,1 +0,0 @@
-# mecha-cms-app
---
+++ backend/src/main/java/io/tribehouse/motomo/MotomoApplication.java
@@ -0,0 +1,13 @@
+package io.tribehouse.motomo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotomoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MotomoApplication.class, args);
+    }
+
+}
