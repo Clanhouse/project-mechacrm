a18d8b05efaf14d4198eac9b9fafa18f7e886ab0
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-09-29 22:22:53 +0200

    [MTO-193] added Switzer Regular as main font

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -1,9 +1,22 @@
+import GlobalStyles from '../src/theme/GlobalStyles';
+import lightTheme from '../src/theme/lightTheme';
+import { ThemeProvider } from 'styled-components';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: {argTypesRegex: '^on[A-Z].*'},
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={lightTheme}>
+      <GlobalStyles/>
+      <Story/>
+    </ThemeProvider>
+  ),
+];
--- frontend/src/theme/GlobalStyles.js
+++ frontend/src/theme/GlobalStyles.js
@@ -1,32 +1,47 @@
 import { createGlobalStyle } from 'styled-components';
+import SwitzerRegularWoff from 'fonts/Switzer-Regular.woff';
+import SwitzerRegularWoff2 from 'fonts/Switzer-Regular.woff2';
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'Switzer';
+    src: url(${SwitzerRegularWoff}) format('woff'),
+    url(${SwitzerRegularWoff2}) format('woff2');
+  }
+
   *, *::before, *::after {
     box-sizing: border-box;
   }
+
   * {
     margin: 0;
     padding: 0;
   }
+
   html, body {
-    font-family: 'Montserrat', sans-serif;
+    font-family: 'Switzer', sans-serif;
     height: 100%;
   }
+
   body {
     -webkit-font-smoothing: antialiased;
     color: ${({ theme }) => theme.color.text.primary};
     background-color: ${({ theme }) => theme.color.background.primary};
   }
+
   img, picture, video, canvas, svg {
     display: block;
     max-width: 100%;
   }
+
   input, button, textarea, select {
     font: inherit;
   }
+
   p, h1, h2, h3, h4, h5, h6 {
     overflow-wrap: break-word;
   }
+
   #root, #__next {
     isolation: isolate;
   }
