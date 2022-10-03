86bf3203b72f898310f549461ec7e7b2df224e91
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-10-04 00:10:27 +0200

    [MTO-187] added initial state Button.js component

--- frontend/src/components/atoms/Button.js
+++ frontend/src/components/atoms/Button.js
@@ -1,14 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Container = styled.button`
-  padding: 10px;
+  border-radius: 8px;
+  padding: 12px 40px;
+
+  color: white;
+
+  background-color: ${({ theme, color }) => {
+    switch (color) {
+      case 'primary':
+        return theme.color.primary;
+      case 'secondary':
+        return theme.color.secondary;
+      default:
+        return 'yellow';
+    }
+  }};
+
+  border: ${({ theme, color }) => {
+    switch (color) {
+      case 'primary':
+        return `2px solid ${theme.color.primary}`;
+      case 'secondary':
+        return `2px solid ${theme.color.secondary}`;
+      default:
+        return `2px solid ${theme.color.primary}`;
+    }
+  }};
 `;
 
-const Button = () => (
-  <Container>
-    <p>button</p>
+const IconBox = styled.p``;
+
+const Text = styled.p``;
+
+const Button = ({ variant, children, Icon, color, size, onClick }) => (
+  <Container variant={variant} color={color} size={size} onClick={onClick}>
+    {Icon && <IconBox>button</IconBox>}
+    <Text>{children}</Text>
   </Container>
 );
+
+Button.propTypes = {
+  color: PropTypes.oneOf([
+    'primary',
+    'secondary',
+    'success',
+    'danger',
+    'warning',
+    'info',
+  ]),
+  variant: PropTypes.oneOf(['normal', 'outlined']),
+  Icon: PropTypes.elementType,
+  size: PropTypes.oneOf(['small', 'normal', 'large']),
+  onClick: PropTypes.func,
+};
+
+Button.defaultProps = {
+  color: 'primary',
+  variant: 'normal',
+  Icon: undefined,
+  size: 'normal',
+  onClick: undefined,
+};
 
 export default Button;
--- frontend/src/components/atoms/Button.stories.js
+++ frontend/src/components/atoms/Button.stories.js
@@ -2,10 +2,35 @@ import React from 'react';
 import Button from './Button';
 
 export default {
-  title: 'Button',
+  title: 'Atoms/Button',
   component: Button,
+  argTypes: {
+    variant: {
+      control: 'inline-radio',
+    },
+  },
 };
 
 const Template = (args) => <Button {...args} />;
 
-export const Primary = Template.bind({});
+export const RegularButton = Template.bind({});
+RegularButton.args = {
+  color: 'primary',
+  children: 'Button',
+  variant: 'normal',
+};
+
+export const RegularButtonWithIcon = Template.bind({});
+RegularButtonWithIcon.args = {
+  color: 'primary',
+  children: 'Button',
+  icon: 'Button',
+  variant: 'normal',
+};
+
+export const IconButton = Template.bind({});
+IconButton.args = {
+  color: 'primary',
+  icon: 'Button',
+  variant: 'normal',
+};
