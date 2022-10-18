80fec300a5787e31272bcc4a8e085fd7909fc464
Dawid <dawid.piatek1995@gmail.com>
2022-10-18 18:25:44 +0200

    [MTO-192, MTO-191, MTO-35] Added Spring Security with JWT and roles support, created user related classes. Authentication via Spring Security and JWT, Authorization via user roles.

---
+++ backend/src/main/java/io/tribehouse/motomo/security/AuthenticationResponse.java
@@ -0,0 +1,8 @@
+package io.tribehouse.motomo.security;
+
+import lombok.Data;
+
+@Data
+public class AuthenticationResponse {
+    private String accessToken;
+}
---
+++ backend/src/main/java/io/tribehouse/motomo/security/JwtRequestFilter.java
@@ -0,0 +1,49 @@
+package io.tribehouse.motomo.security;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    private final JwtTokenService jwtTokenService;
+
+    private final JwtUserDetailsService jwtUserDetailsService;
+
+    public JwtRequestFilter(JwtTokenService jwtTokenService, JwtUserDetailsService jwtUserDetailsService) {
+        this.jwtTokenService = jwtTokenService;
+        this.jwtUserDetailsService = jwtUserDetailsService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (header == null || !header.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        final String token = header.substring(7);
+        final String email = jwtTokenService.validateTokenAndGetEmail(token);//validation token
+        if (email == null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        final UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(email);
+        final UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        filterChain.doFilter(request, response);
+    }
+}
