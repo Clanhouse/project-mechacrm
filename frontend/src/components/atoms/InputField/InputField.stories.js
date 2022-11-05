a91dd0e87721f288a06c5970880cb7d72e371e58
Krzysztof Cholewa <krzysztof.cholewa@gmail.com>
2022-11-05 12:38:32 +0100

    [MTO-195] InputField.js design and stories

--- frontend/src/components/atoms/InputField/InputField.stories.js
+++ frontend/src/components/atoms/InputField/InputField.stories.js
@@ -4,10 +4,52 @@ import InputField from 'components/atoms/InputField/InputField';
 export default {
   title: 'Atoms/InputField',
   component: InputField,
+  argTypes: {
+    type: {
+      control: 'inline-radio',
+    },
+  },
 };
 
 const Template = (args) => <InputField {...args} />;
+const TemplateBox = (args) => (
+  <div style={{ width: '250px' }}>
+    <InputField {...args} />
+  </div>
+);
 
-export const EmptyInput = Template.bind({});
+export const InactiveInput = Template.bind({});
+export const EmailInput = Template.bind({});
+export const ErrorInput = Template.bind({});
+export const InBoxInput = TemplateBox.bind({});
 
-EmptyInput.args = {};
+InactiveInput.args = {
+  id: 'login',
+  label: 'Login',
+  error: false,
+  type: 'text',
+};
+
+EmailInput.args = {
+  id: 'email',
+  label: 'Email',
+  error: false,
+  value: 'example@example.com',
+  type: 'text',
+};
+
+ErrorInput.args = {
+  id: 'password',
+  label: 'Password',
+  error: true,
+  value: 'password',
+  type: 'password',
+};
+
+InBoxInput.args = {
+  id: 'firstname',
+  label: 'Firstname',
+  error: false,
+  value: 'Amanda',
+  type: 'text',
+};
