56ed32c70a4c9efe556941581decd3c3fbaec523
Kris <magikabdul@users.noreply.github.com>
2022-11-07 20:17:48 +0100

    [MTO-195] create input field component (#110)
    * [MTO-195] InputField.js initial state
    
    * [MTO-195] InputField.js design and stories
    
    * [MTO-195] fixed border width and padding

---
+++ frontend/src/components/atoms/InputField/InputField.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.div``;
+
+const Input = styled.input`
+  width: 100%;
+  min-height: 42px;
+
+  padding: 12px 16px;
+
+  font-size: 14px;
+  line-height: 18px;
+
+  outline: none;
+
+  border-radius: 2px;
+  border: ${({ theme, error }) =>
+    error
+      ? `1px solid ${theme.color.error[1]}`
+      : `1px solid ${theme.color.gray[2]}`};
+
+  transition: border 0.3s ease-in-out;
+
+  &:focus,
+  &:hover {
+    border: ${({ theme, error }) =>
+      error
+        ? `1px solid ${theme.color.error[1]}`
+        : `1px solid ${theme.color.primary[1]}`};
+  }
+
+  &::placeholder {
+    color: ${({ theme }) => `${theme.color.gray[3]}`};
+  }
+`;
+
+const InputField = ({ id, type, label, error, value, onChange }) => (
+  <Container>
+    <Input
+      id={id}
+      type={type}
+      placeholder={label}
+      error={error}
+      value={value}
+      onChange={onChange}
+    />
+  </Container>
+);
+
+InputField.propTypes = {
+  id: PropTypes.string.isRequired,
+  type: PropTypes.oneOf(['text', 'password', 'email']),
+  label: PropTypes.string.isRequired,
+  error: PropTypes.bool,
+  value: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+InputField.defaultProps = {
+  type: 'text',
+  error: false,
+  value: undefined,
+  onChange: undefined,
+};
+
+export default InputField;
